2eebc76cd4c9c7f95568b5ea1d896cb90aa36eeb
Joseph <joseph.abuanor@amalitech.com>
2025-03-13 10:15:35 +0000

    change config

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
-export const apiBaseUrl = 'https://xeq4tu82aa.execute-api.eu-central-1.amazonaws.com/Prod';
+export const apiBaseUrl = 'https://rmawjdnkq9.execute-api.eu-central-1.amazonaws.com/Prod';
 
 export const cognitoAuthConfig = {
-  authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_0OqH7QaUV",
-  client_id: "706l8eiv3c11gb7i0eiei9mj7p",
+  authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_KPRV58PNq",
+  client_id: "4vug47lgce7tq11asejl4791oe",
   redirect_uri: window.location.origin,
   response_type: "code",
   scope: "aws.cognito.signin.user.admin email openid",
 };
 
 export const signOutRedirect = () => {
-  const clientId = "706l8eiv3c11gb7i0eiei9mj7p";
+  const clientId = "4vug47lgce7tq11asejl4791oe";
   const logoutUri = "<logout uri>";
-  const cognitoDomain = "https://706l8eiv3c11gb7i0eiei9mj7p.auth.eu-central-1.amazoncognito.com";
+  const cognitoDomain = "https://4vug47lgce7tq11asejl4791oe.auth.eu-central-1.amazoncognito.com";
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
 
